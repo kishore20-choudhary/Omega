be53083663df4655aba7b35155ff1fd73a83e1e3
kishore20-choudhary <69766920+kishore20-choudhary@users.noreply.github.com>
2020-08-17 21:00:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-// Initialize Firebase
-
-
-///Edit Under This only
-
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCMqKykiR2-OR0w6RYELTHw-xiP7zUXFz4",
+  authDomain: "kishore-49135.firebaseapp.com",
+  databaseURL: "https://kishore-49135.firebaseio.com",
+  projectId: "kishore-49135",
+  storageBucket: "kishore-49135.appspot.com",
+  messagingSenderId: "413760728286",
+  appId: "1:413760728286:web:f26b3f256a4babf9d7664f",
+  measurementId: "G-MQ5NL0TYLM"
 };
-
-     
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
